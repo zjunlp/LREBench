8ff4fae9e7af7717750c7cc19c3a417d2b8cd6d4
Xin Xu <xuxin1999102@126.com>
2022-12-19 19:06:09 +0800

    Update dialogue.py

--- data/dialogue.py
+++ data/dialogue.py
@@ -92,49 +92,10 @@ class DataCollatorForSeq2Seq:
 
         return features
 
-
-
-# class DIALOGUE(BaseDataModule):
-#     def __init__(self, args, *wargs) -> None:
-#         super().__init__(args)
-#         self.processor = processors[self.args.task_name](self.args.data_dir, self.args.use_prompt)
-#         self.tokenizer = AutoTokenizer.from_pretrained(self.args.model_name_or_path)
-
-#         self.num_labels = len(self.processor.get_labels())
-
-#         class_list = [f"[class{i}]" for i in range(1, self.num_labels+1)]
-#         num_added_tokens = self.tokenizer.add_special_tokens({'additional_special_tokens': class_list})
-#         unused_list = [f"[unused{i}]" for i in range(1,50)]
-#         num_added_tokens = self.tokenizer.add_special_tokens({'additional_special_tokens': unused_list})
-#         speaker_list = [f"[speaker{i}]" for i in range(1,50)]
-#         num_added_tokens = self.tokenizer.add_special_tokens({'additional_special_tokens': speaker_list})
-#         so_list = ["[sub]", "[obj]"]
-#         num_added_tokens = self.tokenizer.add_special_tokens({'additional_special_tokens': so_list})
-
-
-
-#     def setup(self, stage=None):
-#         self.data_train = get_dataset("train", self.args, self.tokenizer, self.processor)
-#         self.data_val = get_dataset("dev", self.args, self.tokenizer, self.processor)
-#         self.data_test = get_dataset("test", self.args, self.tokenizer, self.processor)
-
-#     def prepare_data(self):
-#         pass
-
-
-#     @staticmethod
-#     def add_to_argparse(parser):
-#         BaseDataModule.add_to_argparse(parser)
-#         parser.add_argument("--task_name", type=str, default="normal", help="[normal, reloss, ptune]")
-#         parser.add_argument("--model_name_or_path", type=str, default="/home/xx/bert-base-uncased", help="Number of examples to operate on per forward step.")
-#         parser.add_argument("--max_seq_length", type=int, default=512, help="Number of examples to operate on per forward step.")
-#         parser.add_argument("--ptune_k", type=int, default=7, help="number of unused tokens in prompt")
-#         return parser
-
 class WIKI80(BaseDataModule):
     def __init__(self, args, model=None) -> None:
         super().__init__(args)
-        self.processor = processors[self.args.task_name](self.args.data_dir, self.args.use_prompt, self.args.labeling)
+        self.processor = processors[self.args.task_name](self.args.data_dir, self.args.use_prompt)
         if self.args.model_name_or_path=="hfl/chinese-roberta-wwm-ext-large":
             self.tokenizer = BertTokenizer.from_pretrained(self.args.model_name_or_path)
         else:
@@ -149,25 +110,22 @@ class WIKI80(BaseDataModule):
         entity_list = ["[object_start]", "[object_end]", "[subject_start]", "[subject_end]"]
         class_list = [f"[class{i}]" for i in range(1, self.num_labels+1)]
 
-        num_added_tokens = self.tokenizer.add_special_tokens({'additional_special_tokens': entity_list})
-        num_added_tokens = self.tokenizer.add_special_tokens({'additional_special_tokens': class_list})
+        # num_added_tokens = self.tokenizer.add_special_tokens({'additional_special_tokens': entity_list})
+        # num_added_tokens = self.tokenizer.add_special_tokens({'additional_special_tokens': class_list})
         if use_gpt:
             self.tokenizer.add_special_tokens({'cls_token': "[CLS]"})
             self.tokenizer.add_special_tokens({'pad_token': '[PAD]'})
         so_list = ["[sub]", "[obj]"]
-        num_added_tokens = self.tokenizer.add_special_tokens({'additional_special_tokens': so_list})
+        # num_added_tokens = self.tokenizer.add_special_tokens({'additional_special_tokens': so_list})
 
         prompt_tokens = [f"[T{i}]" for i in range(1,6)]
-        self.tokenizer.add_special_tokens({'additional_special_tokens': prompt_tokens})
-        
-        
-
-
+        # num_added_tokens = self.tokenizer.add_special_tokens({'additional_special_tokens': prompt_tokens})
+        self.tokenizer.add_special_tokens({'additional_special_tokens': entity_list + class_list + so_list + prompt_tokens})
 
 
     def setup(self, stage=None):
         self.data_train = get_dataset("train", self.args, self.tokenizer, self.processor)
-        # self.data_val = get_dataset("dev", self.args, self.tokenizer, self.processor)
+        self.data_val = get_dataset("dev", self.args, self.tokenizer, self.processor)
         self.data_test = get_dataset("test", self.args, self.tokenizer, self.processor)
 
 
